2729c7eb2035d8641fa231308abe2ee1dbded07c
Aleksandr Syschenko <asyschenko@gmail.com>
2019-01-09 11:05:54 +0200

    project was created

---
+++ TouchHandlerLabel/TouchHandlerLabel.h
@@ -0,0 +1,19 @@
+//
+//  TouchHandlerLabel.h
+//  TouchHandlerLabel
+//
+//  Created by Aleksandr Syschenko on 1/9/19.
+//  Copyright © 2019 Aleksandr Syschenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TouchHandlerLabel.
+FOUNDATION_EXPORT double TouchHandlerLabelVersionNumber;
+
+//! Project version string for TouchHandlerLabel.
+FOUNDATION_EXPORT const unsigned char TouchHandlerLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TouchHandlerLabel/PublicHeader.h>
+
+
---
+++ TouchHandlerLabelExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchHandlerLabelExample
+//
+//  Created by Aleksandr Syschenko on 1/9/19.
+//  Copyright © 2019 Aleksandr Syschenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
