60a80bc82cf81eb9ebeac7f3a518b56445417b22
Aleksandr Syschenko <asyschenko@gmail.com>
2019-01-09 11:34:12 +0200

    Tests and example were added

--- TouchHandlerLabelExample/ViewController.swift
+++ TouchHandlerLabelExample/ViewController.swift
@@ -7,14 +7,94 @@
 //
 
 import UIKit
+import TouchHandlerLabel
+
+class CustomTouchHandler: TouchHandler {
+    
+    func rangesFor(text: String) -> [NSRange] {
+        if let range = text.range(of: "Custom touch handler") {
+            return [NSRange(location: range.lowerBound.encodedOffset,
+                            length: range.upperBound.encodedOffset - range.lowerBound.encodedOffset)]
+        }
+        return [NSRange]()
+    }
+    
+    func attributesForNormalStateBy(range: NSRange) -> [NSAttributedString.Key: Any] {
+        return [NSAttributedString.Key.foregroundColor: UIColor.blue,
+                NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue,
+                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15.0, weight: .bold)]
+    }
+    
+    func attributesForSelectedStateBy(range: NSRange) -> [NSAttributedString.Key : Any] {
+        return [NSAttributedString.Key.foregroundColor: UIColor.blue,
+                NSAttributedString.Key.underlineStyle: NSUnderlineStyle.double.rawValue,
+                NSAttributedString.Key.font: UIFont.italicSystemFont(ofSize: 15)]
+    }
+    
+    func handleClickBy(range: NSRange, text: String) {
+        print("Custom touch handler:", text)
+    }
+    
+}
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: TouchHandlerLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let rangesTouchHandler = RangesTouchHandler(ranges: [NSRange(location: 0, length: 11), NSRange(location: 13, length: 12)]) { (range, str) in
+            print("Range:", range)
+            print("String:", str)
+        }
+        
+        rangesTouchHandler.normalColor = UIColor.blue
+        rangesTouchHandler.selectedColor = UIColor.blue.withAlphaComponent(0.5)
+        label.add(touchHandler: rangesTouchHandler)
+        
+        let simpleTextHandler = SimpleTextTouchHandler(text: "text") { (_, str) in
+            print("Simple text:", str)
+        }
+        
+        simpleTextHandler.normalColor = UIColor.red
+        simpleTextHandler.selectedColor = UIColor.red.withAlphaComponent(0.5)
+        label.add(touchHandler: simpleTextHandler)
+        
+        let urlHandler = URLTouchHandler { (_, str) in
+            print("URL:",str)
+        }
+        
+        urlHandler.normalColor = UIColor.green
+        urlHandler.selectedColor = UIColor.green.withAlphaComponent(0.5)
+        label.add(touchHandler: urlHandler)
+        
+        let emailHandler = EmailTouchHandler { (_, str) in
+            print("Email:",str)
+        }
+        
+        emailHandler.normalColor = UIColor.gray
+        emailHandler.selectedColor = UIColor.gray.withAlphaComponent(0.5)
+        label.add(touchHandler: emailHandler)
+        
+        let phoneNumbersHandler = PhoneNumberTouchHandler { (_, str) in
+            print("Phone number:",str)
+        }
+        
+        phoneNumbersHandler.normalColor = UIColor.magenta
+        phoneNumbersHandler.selectedColor = UIColor.magenta.withAlphaComponent(0.5)
+        label.add(touchHandler: phoneNumbersHandler)
+        
+        let hashtgsHandler = HashtagTouchHandler { (_, str) in
+            print("Hashtag:",str)
+        }
+        
+        hashtgsHandler.normalColor = UIColor.brown
+        hashtgsHandler.selectedColor = UIColor.brown.withAlphaComponent(0.5)
+        label.add(touchHandler: hashtgsHandler)
+        
+        label.add(touchHandler: CustomTouchHandler())
     }
-
-
+    
 }
 
--- TouchHandlerLabelTests/TouchHandlerLabelTests.swift
+++ TouchHandlerLabelTests/TouchHandlerLabelTests.swift
@@ -9,26 +9,239 @@
 import XCTest
 @testable import TouchHandlerLabel
 
-class TouchHandlerLabelTests: XCTestCase {
-
+class BasicTouchHandlerTest: XCTestCase {
+    
+    private let testString = "Test string"
+    private let testRange = NSRange(location: 0, length: 11)
+    
+    
+    private var touchHandler: TouchHandler?
+    private var handlerCallResult = false
+    private var handlerParametersResult = false
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        touchHandler = BasicTouchHandler(handler: { [weak self] (range, str) in
+            guard let weakSelf = self else {
+                return
+            }
+            weakSelf.handlerCallResult = true
+            
+            if range == weakSelf.testRange && str == weakSelf.testString {
+                weakSelf.handlerParametersResult = true
+            }
+        })
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testCallHandler() {
+        touchHandler?.handleClickBy(range: testRange, text: testString)
+        XCTAssertTrue(handlerCallResult, "Handler was not called")
+        XCTAssertTrue(handlerParametersResult, "Parameters of handler were not matched")
     }
+    
+}
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+class RangesTouchHandlerTest: XCTestCase {
+    
+    private let testRanges = [NSRange(location: 0, length: 1), NSRange(location: 2, length: 2), NSRange(location: 5, length: 10)]
+    private var touchHandler: TouchHandler?
+    
+    override func setUp() {
+        touchHandler = RangesTouchHandler(ranges: testRanges)
     }
+    
+    func testRangesParameters() {
+        let result = testRanges == touchHandler?.rangesFor(text: "")
+        
+        XCTAssertTrue(result, "Ranges were not matched")
+    }
+    
+}
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class SimpleTextTouchHandlerTest: XCTestCase {
+    
+    private let testString = "Test, test, test, Test, Test, some text"
+    private var touchHandlerCaseInsensitiveFirstMatching: TouchHandler?
+    private var touchHandlerNoCaseInsensitiveFirstMatching: TouchHandler?
+    private var touchHandlerCaseInsensitiveNoFirstMatching: TouchHandler?
+    private var touchHandlerNoCaseInsensitiveNoFirstMatchingUppercase: TouchHandler?
+    private var touchHandlerNoCaseInsensitiveNoFirstMatchingLowercase: TouchHandler?
+    
+    override func setUp() {
+        touchHandlerCaseInsensitiveFirstMatching = SimpleTextTouchHandler(text: "Test", caseInsensitive: true, firstMatching: true)
+        touchHandlerNoCaseInsensitiveFirstMatching = SimpleTextTouchHandler(text: "test", caseInsensitive: false, firstMatching: true)
+        touchHandlerCaseInsensitiveNoFirstMatching = SimpleTextTouchHandler(text: "Test", caseInsensitive: true, firstMatching: false)
+        touchHandlerNoCaseInsensitiveNoFirstMatchingUppercase = SimpleTextTouchHandler(text: "Test", caseInsensitive: false, firstMatching: false)
+        touchHandlerNoCaseInsensitiveNoFirstMatchingLowercase = SimpleTextTouchHandler(text: "test", caseInsensitive: false, firstMatching: false)
+    }
+    
+    func testSearchTextWhereCaseInsensitiveAndFirstMatching() {
+        let ranges = touchHandlerCaseInsensitiveFirstMatching?.rangesFor(text: testString)
+        let firstRange = ranges?.first ?? NSRange()
+        
+        XCTAssertEqual(ranges?.count ?? 0, 1, "Ranges count test failed")
+        XCTAssertTrue(firstRange.location == 0 && firstRange.length == 4, "Ranges matching test failed")
+    }
+    
+    func testSearchTextWhereNoCaseInsensitiveAndFirstMatching() {
+        let ranges = touchHandlerNoCaseInsensitiveFirstMatching?.rangesFor(text: testString)
+        let firstRange = ranges?.first ?? NSRange()
+        
+        XCTAssertEqual(ranges?.count ?? 0, 1, "Ranges count test failed")
+        XCTAssertTrue(firstRange.location == 6 && firstRange.length == 4, "Ranges matching test failed")
+    }
+    
+    func testSearchTextWhereCaseInsensitiveAndNoFirstMatching() {
+        let ranges = touchHandlerCaseInsensitiveNoFirstMatching?.rangesFor(text: testString)
+        
+        XCTAssertEqual(ranges?.count ?? 0, 5, "Ranges count test failed")
+    }
+    
+    func testSearchTextWhereNoCaseInsensitiveAndNoFirstMatching() {
+        let rangesUppercase = touchHandlerNoCaseInsensitiveNoFirstMatchingUppercase?.rangesFor(text: testString)
+        let rangesLowwercase = touchHandlerNoCaseInsensitiveNoFirstMatchingLowercase?.rangesFor(text: testString)
+        
+        XCTAssertEqual(rangesUppercase?.count ?? 0, 3, "Ranges count test failed")
+        XCTAssertEqual(rangesLowwercase?.count ?? 0, 2, "Ranges count test failed")
+    }
+    
+}
+
+class RegExpTouchHandlerTest: XCTestCase {
+    
+    private let testString = "Regual expression test string, 1234 numbers container (1234), (4321), four4, 5five, 1, 2, 4"
+    private var touchHandlerFirstMatching: TouchHandler?
+    private var touchHandlerNoFirstMatching: TouchHandler?
+    
+    override func setUp() {
+        guard let regExp = try? NSRegularExpression(pattern: "\\([\\d]+\\)", options: []) else {
+            return
         }
+        touchHandlerFirstMatching = RegExpTouchHandler(regExp: regExp, firstMatching: true)
+        touchHandlerNoFirstMatching = RegExpTouchHandler(regExp: regExp, firstMatching: false)
     }
+    
+    func testSearchTextWithFirstMatching() {
+        let ranges = touchHandlerFirstMatching?.rangesFor(text: testString)
+        let firstRange = ranges?.first ?? NSRange()
+        
+        XCTAssertEqual(ranges?.count ?? 0, 1, "Ranges count test failed")
+        XCTAssertTrue(firstRange.location == 54 && firstRange.length == 6, "Ranges matching test failed")
+    }
+    
+    func testSearchTextWithoutFirstMatching() {
+        let ranges = touchHandlerNoFirstMatching?.rangesFor(text: testString)
+        
+        XCTAssertEqual(ranges?.count ?? 0, 2, "Ranges count test failed")
+    }
+    
+}
 
+class URLTouchHandlerTest: XCTestCase {
+    
+    private let testString = "https://translate.google.ru " +
+        "https://www.apple.com " +
+        "https://www.apple.com/shop/buy-iphone/iphone-8 " +
+        "https://www.google.com/ " +
+        "http://www.fake-domen.com/ " +
+        "http://login:password@host:128/path?param1=1&param2=2#ancher" +
+    "http:\\ww.incorrect-domen"
+    private var touchHandlerFirstMatching: TouchHandler?
+    private var touchHandlerNoFirstMatching: TouchHandler?
+    
+    override func setUp() {
+        touchHandlerFirstMatching = URLTouchHandler(firstMatching: true)
+        touchHandlerNoFirstMatching = URLTouchHandler(firstMatching: false)
+    }
+    
+    func testSearchTextWithFirstMatching() {
+        let ranges = touchHandlerFirstMatching?.rangesFor(text: testString)
+        
+        XCTAssertEqual(ranges?.count ?? 0, 1, "Ranges count test failed")
+    }
+    
+    func testSearchTextWithoutFirstMatching() {
+        let ranges = touchHandlerNoFirstMatching?.rangesFor(text: testString)
+        
+        print(testString)
+        XCTAssertEqual(ranges?.count ?? 0, 6, "Ranges count test failed")
+    }
+    
+}
+
+class PhoneNumberTouchHandlerTest: XCTestCase {
+    
+    private let testString = "+111 (11) 111 11 11, +222222222222, +333(33)3333333, 242344223"
+    private var touchHandlerFirstMatching: TouchHandler?
+    private var touchHandlerNoFirstMatching: TouchHandler?
+    
+    override func setUp() {
+        touchHandlerFirstMatching = PhoneNumberTouchHandler(firstMatching: true)
+        touchHandlerNoFirstMatching = PhoneNumberTouchHandler(firstMatching: false)
+    }
+    
+    func testSearchTextWithFirstMatching() {
+        let ranges = touchHandlerFirstMatching?.rangesFor(text: testString)
+        
+        XCTAssertEqual(ranges?.count ?? 0, 1, "Ranges count test failed")
+    }
+    
+    func testSearchTextWithoutFirstMatching() {
+        let ranges = touchHandlerNoFirstMatching?.rangesFor(text: testString)
+        
+        print(testString)
+        XCTAssertEqual(ranges?.count ?? 0, 3, "Ranges count test failed")
+    }
+    
+}
+
+class EmailTouchHandlerTest: XCTestCase {
+    
+    private let testString = "some_mail@mail.com, somemail@gmail.com, SOME_M_A_I_L@mail.com, incorrect-mail.com"
+    private var touchHandlerFirstMatching: TouchHandler?
+    private var touchHandlerNoFirstMatching: TouchHandler?
+    
+    override func setUp() {
+        touchHandlerFirstMatching = EmailTouchHandler(firstMatching: true)
+        touchHandlerNoFirstMatching = EmailTouchHandler(firstMatching: false)
+    }
+    
+    func testSearchTextWithFirstMatching() {
+        let ranges = touchHandlerFirstMatching?.rangesFor(text: testString)
+        
+        XCTAssertEqual(ranges?.count ?? 0, 1, "Ranges count test failed")
+    }
+    
+    func testSearchTextWithoutFirstMatching() {
+        let ranges = touchHandlerNoFirstMatching?.rangesFor(text: testString)
+        
+        print(testString)
+        XCTAssertEqual(ranges?.count ?? 0, 3, "Ranges count test failed")
+    }
+    
+}
+
+class HashtagTouchHandlerTest: XCTestCase {
+    
+    private let testString = "#hashtag, #hashtag2, #2hashtag_incorrect"
+    private var touchHandlerFirstMatching: TouchHandler?
+    private var touchHandlerNoFirstMatching: TouchHandler?
+    
+    override func setUp() {
+        touchHandlerFirstMatching = HashtagTouchHandler(firstMatching: true)
+        touchHandlerNoFirstMatching = HashtagTouchHandler(firstMatching: false)
+    }
+    
+    func testSearchTextWithFirstMatching() {
+        let ranges = touchHandlerFirstMatching?.rangesFor(text: testString)
+        
+        XCTAssertEqual(ranges?.count ?? 0, 1, "Ranges count test failed")
+    }
+    
+    func testSearchTextWithoutFirstMatching() {
+        let ranges = touchHandlerNoFirstMatching?.rangesFor(text: testString)
+        
+        print(testString)
+        XCTAssertEqual(ranges?.count ?? 0, 2, "Ranges count test failed")
+    }
+    
 }
