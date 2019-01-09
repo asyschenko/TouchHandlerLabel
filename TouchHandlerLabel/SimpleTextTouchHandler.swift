9b6410ae03d4fc93ca3b09f0d78f949d5f5e15bc
Aleksandr Syschenko <asyschenko@gmail.com>
2019-01-09 11:16:18 +0200

    RegExpTouchHandler was added

--- TouchHandlerLabel/SimpleTextTouchHandler.swift
+++ TouchHandlerLabel/SimpleTextTouchHandler.swift
@@ -8,27 +8,37 @@
 
 import Foundation
 
-open class RegExpTouchHandler: BasicTouchHandler {
+open class SimpleTextTouchHandler: BasicTouchHandler {
     
-    open private (set) var regExp: NSRegularExpression
+    open private (set) var text: String
     open private (set) var firstMatching: Bool
+    open private (set) var caseInsensitive: Bool
     
-    public init(regExp: NSRegularExpression, firstMatching: Bool = true, handler: Handler? = nil) {
-        self.regExp = regExp
+    public init(text: String, caseInsensitive: Bool = true, firstMatching: Bool = true, handler: Handler? = nil) {
+        self.text = text
         self.firstMatching = firstMatching
+        self.caseInsensitive = caseInsensitive
         super.init(handler: handler)
     }
     
     open override func rangesFor(text: String) -> [NSRange] {
-        let result = regExp.matches(in: text, options: [], range: NSRange(location: 0, length: text.count))
+        var ranges = [Range<String.Index>]()
+        let options: String.CompareOptions = caseInsensitive ? [String.CompareOptions.caseInsensitive] : []
         
-        if let first = result.first {
+        while let range = text.range(of: self.text, options: options, range: (ranges.last?.upperBound ?? text.startIndex)..<text.endIndex) {
+            ranges.append(range)
+        }
+        
+        let returnRanges = ranges.map({ (currentRange) -> NSRange in
+            return NSRange(location: currentRange.lowerBound.encodedOffset,
+                           length: currentRange.upperBound.encodedOffset - currentRange.lowerBound.encodedOffset)
+        })
+        
+        if let first = returnRanges.first {
             if firstMatching {
-                return [first.range]
+                return [first]
             }
-            return result.map({ (resultItem) -> NSRange in
-                return resultItem.range
-            })
+            return returnRanges
         }
         return [NSRange]()
     }
